624ea89d457b50aacf7490bbd6a29417394aab61
Namrata <namratabose32@gmail.com>
2024-01-08 00:34:29 +0530

    feat: Add new blog page created

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const path=require('path');
 const express=require('express')
 const app=express();
 const userRoute=require('./routes/user')
+const blogRoute=require('./routes/blog')
+const Blog=require('./models/blog')
 const mongoose=require("mongoose")
 const cookieParser=require('cookie-parser');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
@@ -14,14 +16,17 @@ app.set("views",path.resolve("./views"));
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')))
 
-
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
+    const allBlogs=await Blog.find({});
     res.render("home",{
         user:req.user,
+        blogs:allBlogs,
     })
 })
 
 app.use("/user",userRoute);
+app.use("/blog",blogRoute);
 
 app.listen(PORT,()=>console.log(`Server started at PORT:${PORT}`));
