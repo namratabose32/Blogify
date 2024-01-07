d8920892e79bed09c24b47986daf5b71db49157e
Namrata <namratabose32@gmail.com>
2024-01-07 18:13:06 +0530

    feat: SignUp apiimplemented with route

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const path=require('path');
 const express=require('express')
 const app=express();
-
+const userRoute=require('./routes/user')
+const mongoose=require("mongoose")
 const PORT=8000;
+
+mongoose.connect("mongodb+srv://namratabose32:namratabose32@cluster0.eproxgu.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("MongoDb connected"))
+
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
-
+app.use(express.urlencoded({extended:false}))
 app.get('/',(req,res)=>{
     res.render("home")
 })
+
+app.use("/user",userRoute);
+
 app.listen(PORT,()=>console.log(`Server started at PORT:${PORT}`));
